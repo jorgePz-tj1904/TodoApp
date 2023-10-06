e3001a013f53774a4c367d58898f7a1928053a7d
jorgePz-tj1904 <jlgames1904@gamil.com>
2023-10-06 19:04:09 -0300

    dominio

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,13 +12,13 @@ const HomePage = () => {
         throw new Error('Failed to fetch data');
       }
       const data = await response.json();
-      
-      if(data.tasks.length > 0){
+
+      if (data.tasks.length > 0) {
         setTasks(data.tasks);
-      }else{
+      } else {
         setTasks(['null']);
       }
-      
+
       localStorage.setItem('userId', data.id);
       console.log(tasks);
     } catch (error) {
@@ -33,17 +33,25 @@ const HomePage = () => {
 
   return (
     <div className="relative">
-  <div className="mt-10">
-    <img
-      src="https://i.ibb.co/Nyrrmcm/My-project-1-2.png"
-      className="w-40 mx-auto mb-10 md:w-52"
-      alt="Logo"
-    />
-  </div>
-  <div>
-    <Tasks tasks={tasks} />
-  </div>
-</div>
+      <div className="mt-10">
+        <img
+          src="https://i.ibb.co/Nyrrmcm/My-project-1-2.png"
+          className="w-40 mx-auto mb-10 md:w-52"
+          alt="Logo"
+        />
+      </div>
+      <div>
+        <Tasks tasks={tasks} />
+      </div>
+      <footer className=' h-8 flex justify-center mt-24 mb-20'>
+        <div>
+          <a href="https://github.com/jorgePz-tj1904/TodoApp">
+            <img className=' mx-auto' width={70} src="./github.png" alt="" />
+          </a>
+          <p>Derechos Reservados ©</p>
+        </div>
+      </footer>
+    </div>
   );
 };
 
